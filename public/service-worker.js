31f44dd1908a444405c8750ad7362347548b4f13
Michael <mbahl1670@gmail.com>
2022-02-20 12:44:52 -0600

    added manifest.json file

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,6 +6,7 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
+  '/manifest.json',
   '/css/styles.css',
   '/icons/icon-72x72.png',
   '/icons/icon-96x96.png',
